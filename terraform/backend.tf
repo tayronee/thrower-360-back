fb6cb21e9d34d4151c0831b11c8d3048b863b1df
Tayrone <tayrone@MacBook-Pro-de-Tayrone-2.local>
2025-07-05 21:34:05 -0300

    Fix Terraform backend storage account name and add ARM environment variables

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "stthrower360terraform"
+    storage_account_name = "stthrower1751761535"
     container_name       = "tfstate"
     key                  = "thrower-360-back.tfstate"
   }
